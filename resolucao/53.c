05c1caace01ec1df2330ff026323f986985703cb
backup-unifesp <vinicius.coral@unifesp.br>
2026-04-14 14:54:14 -0300

    Add program to print numbers up to user input

---
+++ resolucao/53.c
@@ -0,0 +1,20 @@
+// 53)
+#include <stdio.h>
+
+int main() {
+    int count, n;
+    while(1) {
+        printf("\nDigite o último valor: ");
+        scanf("%d", &n);
+        if (n>0) {
+            for(count=1; count<=n; count++) {
+                printf("| %d ", count);
+        }
+        break;
+        }
+        else {
+            printf("O valor deve ser maior que zero...\n");
+        }
+    }
+    return 0;
+}
